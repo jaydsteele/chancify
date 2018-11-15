ae23696e6d6c6f471873960927c5fb9d49adb874
Jay Steele <jay.steele@velo.team>
2018-11-15 13:52:34 -0400

    Get the generator working with tests

--- app/generate.js
+++ app/generate.js
@@ -1,5 +1,41 @@
-function generate(/* template */) {
-  return true
+const _ = require('lodash')
+
+// Set an upper bound on the size of a list resource
+const MAX_LIST_RESOURCE_SIZE = 10000
+
+function generateResource(template) {
+  function valueCloner(value) {
+    if (_.isFunction(value)) {
+      return value()
+    }
+    return undefined
+  }
+  if (!template) {
+    return undefined
+  }
+  return _.cloneDeepWith(template, valueCloner)
 }
 
-module.exports.generate = generate
+function generateListResource(template, options) {
+  if (!template) {
+    return []
+  }
+  if (_.get(options, 'max') > MAX_LIST_RESOURCE_SIZE) {
+    throw new Error(`options.max cannot be larger than ${MAX_LIST_RESOURCE_SIZE}`)
+  }
+  const opts = _.defaults(options, {
+    skip: 0,
+    limit: 10,
+    max: 10,
+  })
+  // to ensure seeded random resource generators will always return the same
+  // set of values, we will actually generate our entire resource (max items)
+  // and return the array representing our paging window
+  const entireResource = _.times(opts.max, () => generateResource(template))
+  // now discard any items outside of the page range
+  const result = entireResource.slice(opts.skip, opts.skip + opts.limit)
+  return result
+}
+
+module.exports.generateResource = generateResource
+module.exports.generateListResource = generateListResource
